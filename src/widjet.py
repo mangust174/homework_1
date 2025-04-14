90f2d717e6025c4d0181cb5237cffff7471b0b3e
Максим Лашкин <beautifulsunrise174@gmail.com>
2025-04-14 11:09:29 +0300

    widjet, доработка

--- src/widjet.py
+++ src/widjet.py
@@ -1,21 +1,5 @@
 from datetime import datetime
-
-
-def mask_account_card(cart_number: str) -> str:
-    """Функция принимает номер карты (строка) и возвращает её маску"""
-    """проверяем на случай,если днина номера отличается"""
-    if len(cart_namber) != 16:
-        """если цифр больше маскируем по такому варианту"""
-        return cart_namber[:4] + "****" + cart_namber[-4:]
-        """если цифр 16"""
-    return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
-
-
-def mask_account_number(account_number: str) -> str:
-    """если номер короче"""
-    if len(account_number) < 4:
-        return "**" + account_number
-    return "**" + account_number[-4:]
+from masks import mask_card_number, mask_account_number
 
 
 def mask_account_card(data: str) -> str:
@@ -24,7 +8,7 @@ def mask_account_card(data: str) -> str:
     if len(parts) != 2:
         return data
 
-first_part, number_part = parts
+    first_part, number_part = parts
     # Проверяем, является ли это счётом:
     if first_part.split().lower().startswith("счет"):
         masked_number = mask_account_number(number_part)
