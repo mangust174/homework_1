aed2e29289ed435b66263991f4e2c5b7afaa7882
Максим Лашкин <beautifulsunrise174@gmail.com>
2025-04-16 16:50:46 +0300

    widjet, исправление

--- src/widjet.py
+++ src/widjet.py
@@ -1,8 +1,9 @@
+from datetime import datetime
 from src.masks import get_mask_card_number, get_mask_account
 
 def mask_account_card(data: str) -> str:
     """Функция, принимающая информацию о карте или счете"""
-    parts = data.split(' ', 1)
+    parts = data.rsplit(' ', 1)
     if len(parts) != 2:
         return data
 
@@ -21,8 +22,8 @@ def get_date(date_str: str) -> str:
     Принимает дату в ISO-формате (например, "2024-03-11T02:26:18.671407")
     и возвращает дату в формате "ДД.ММ.ГГГГ".
     """
-    from datetime import datetime
     date_obj = datetime.fromisoformat(date_str)
     return date_obj.strftime("%d.%m.%Y")
 
-print(mask_account_card("Visa Platinum 7000792289606361"))
+if __name__ == "__main__":
+    print(mask_account_card("Visa Platinum 7000792289606361"))
