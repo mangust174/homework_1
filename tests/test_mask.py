3c579a610cb66ed691105276a0c9c12c1abc6119
Максим Лашкин <beautifulsunrise174@gmail.com>
2025-05-01 17:46:52 +0300

    с докстрингами

--- tests/test_mask.py
+++ tests/test_mask.py
@@ -1,19 +1,20 @@
 from src.masks import get_mask_card_number, get_mask_account
-
 from datetime import datetime
-
 import pytest
 
 
 @pytest.mark.parametrize("card_number,expected", valid_card_numbers())
 def test_get_mask_card_number(card_number, expected):
+    """ тест на правильность маскировки номера карты """
     assert get_mask_card_number(card_number) == expected
 
 @pytest.mark.parametrize("account_number,expected", valid_accounts())
 def test_get_mask_account(account_number, expected):
+    """ тест на правильность маскировки номера счета """
     assert get_mask_account(account_number) == expected
 
 def test_get_date(date_data):
+    """ тест проверки преобразования даты """
     if isinstance(date_data["valid"], tuple):
         date_str, expected_result = date_data["valid"]
         result = get_date(date_str)
