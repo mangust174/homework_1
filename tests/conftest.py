9bef5a1a636decc138f9875f93f914e56a82afde
Максим Лашкин <beautifulsunrise174@gmail.com>
2025-05-01 17:42:11 +0300

    с докстрингами

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,7 @@ import pytest
 
 
 @pytest.fixture(scope="module")
+""" карточные номера """
 def valid_card_numbers():
     return [
         ("1234567890123456", "1234 56** **** 3456"),
@@ -11,11 +12,13 @@ def valid_card_numbers():
 
 
 @pytest.fixture(scope="module")
+""" неверные номера """
 def invalid_card_number():
     return ["123456789012345", "abcdef"]
 
 
 @pytest.fixture(scope="module")
+""" номера счетов """
 def valid_accounts():
     return [
         ("1234567890", "**90"),
@@ -25,13 +28,15 @@ def valid_accounts():
 
 
 @pytest.fixture(scope="module")
+""" неверные счета """
 def invalid_accounts():
     return ["abcde", "123"]
 
 
 @pytest.fixture(scope="module")
+""" тест даты """
 def date_data():
     return {
-        "valid": ("2024-03-11T02:26:18.671407", "11.03.2024"),
-        "invalid": ("not-a-date", None),
+        "valid": ("2024-03-11T02:26:18.671407", "11.03.2024"), # дата
+        "invalid": ("not-a-date", None), # неверная дата
     }
